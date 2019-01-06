15fc309b23893f34ed78f0a1e8e990400c30219c
ahavoja <ahavoja@gmail.com>
2019-01-06 20:13:34 +0200

    fix typo

--- TMC2130_tests.ino
+++ TMC2130_tests.ino
@@ -1,5 +1,6 @@
-// Arduino Nano pin connections. These you can rewire freely. Each TMC2130 needs its own pins. To reduce the number of pins needed, i have wired EN and DIR to GND.
-#define EN_PIN    0xFFFF  //enable (CFG6). I want driver always enabled, so connect EN -- GND 
+// Arduino Nano pin connections. These you can rewire freely. Each TMC2130 needs its own pins.
+// To reduce the number of pins needed, i have wired EN and DIR to GND.
+#define EN_PIN    0xFFFF  //enable (CFG6). I want driver always enabled, so connect EN --> GND 
 #define DIR_PIN   0xFFFF //direction can also be controlled through SPI, so connect DIR --> GND
 #define STEP_PIN  A4 	//step
 #define CS_PIN    A3 	//chip select
