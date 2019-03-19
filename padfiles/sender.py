bf4c730bb58634a553c9c716964a4b0f79e86fa2
Łukasz Michowski <38592150+lukaszbos@users.noreply.github.com>
2019-03-19 23:13:47 +0100

    python software for pads

---
+++ padfiles/sender.py
@@ -0,0 +1,27 @@
+# Class used to send data from pad.py to AC software
+
+
+class Sender:
+
+    def __init__(self, joystickNumber, voltage):
+        self.joystickNumber = joystickNumber
+        self.voltage = voltage
+
+    def setVoltage(self, joystickNumber, voltage):
+        self.joystickNumber = joystickNumber
+        self.voltage = voltage
+
+    def getVoltage(self, voltage):
+        return voltage
+
+    def printCurrentVoltage(self):
+       # print("Current voltage is: ")
+        #print(self.joystickNumber)
+        print(self.voltage)
+
+
+
+# sender = Sender(voltage=0)
+# pad.sender.printCurrentVoltage()
+# pad.sender.setVoltage(0.4)
+# pad.sender.printCurrentVoltage()
